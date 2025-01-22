6a22b6929ff144f4b5f7a7554b5c81c058a0a7fb
chhayabangia <chhayabangia@gmail.com>
2025-01-21 23:41:12 -0500

    updated code

--- prompts.js
+++ prompts.js
@@ -44,42 +44,44 @@ const mainMenu = async () => {
     case "View all employees":
       console.table(await getEmployees());
       break;
-      case "Add a department":
-        const response = await inquirer.prompt([{ type: 'input', name: "dept_name", message: "Enter department name:" }]);
-        const name = response.name;
-        await addDepartment(name);
-        console.log("Department added!");
-        break;
+    case "Add a department":
+      const response = await inquirer.prompt([
+        { type: "input", name: "dept_name", message: "Enter department name:" },
+      ]);
+      const dept_name = response.dept_name;  // Correcting this to use `dept_name`
+      await addDepartment(dept_name);  // Pass the correct variable to the function
+      console.log("Department added!");
+      break;
     case "Add a role":
-    const roleResponse = await inquirer.prompt([
-        { type: 'input', name: "title", message: "Enter role title:" },
-        { type: 'input', name: "salary", message: "Enter salary:" },
-        { type: 'input', name: "department_id", message: "Enter department ID for this role:" }
-    ]);
-    const { title, salary, department_id } = roleResponse;
-    await addRole(title, salary, department_id);
-    console.log("Role added!");
-    break;
+      const roleResponse = await inquirer.prompt([
+        { type: "input", name: "title", message: "Enter role title:" },
+        { type: "input", name: "salary", message: "Enter salary:" },
+        { type: "input", name: "department_id", message: "Enter department ID for this role:" },
+      ]);
+      const { title, salary, department_id } = roleResponse;
+      await addRole(title, salary, department_id);
+      console.log("Role added!");
+      break;
     case "Add an employee":
-    const employeeResponse = await inquirer.prompt([
-        { type: 'input', name: "first_name", message: "Enter employee's first name:" },
-        { type: 'input', name: "last_name", message: "Enter employee's last name:" },
-        { type: 'input', name: "role_id", message: "Enter role ID:" },
-        { type: 'input', name: "manager_id", message: "Enter manager's ID (if any):" }
-    ]);
-    const { first_name, last_name, role_id, manager_id } = employeeResponse;
-    await addEmployee(first_name, last_name, role_id, manager_id);
-    console.log("Employee added!");
-    break;
+      const employeeResponse = await inquirer.prompt([
+        { type: "input", name: "first_name", message: "Enter employee's first name:" },
+        { type: "input", name: "last_name", message: "Enter employee's last name:" },
+        { type: "input", name: "role_id", message: "Enter role ID:" },
+        { type: "input", name: "manager_id", message: "Enter manager's ID (if any):" },
+      ]);
+      const { first_name, last_name, role_id, manager_id } = employeeResponse;
+      await addEmployee(first_name, last_name, role_id, manager_id);
+      console.log("Employee added!");
+      break;
     case "Update an employee role":
-    const updateResponse = await inquirer.prompt([
-        { type: 'input', name: "employee_id", message: "Enter employee ID to update:" },
-        { type: 'input', name: "new_role_id", message: "Enter new role ID:" }
-    ]);
-    const { employee_id, new_role_id } = updateResponse;
-    await updateEmployeeRole(employee_id, new_role_id);
-    console.log("Employee role updated!");
-    break;
+      const updateResponse = await inquirer.prompt([
+        { type: "input", name: "employee_id", message: "Enter employee ID to update:" },
+        { type: "input", name: "new_role_id", message: "Enter new role ID:" },
+      ]);
+      const { employee_id, new_role_id } = updateResponse;
+      await updateEmployeeRole(employee_id, new_role_id);
+      console.log("Employee role updated!");
+      break;
     case "Exit":
       console.log("Goodbye!");
       process.exit();
--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ CREATE DATABASE employee_tracker;
 
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(30) UNIQUE NOT NULL
+    dept_name VARCHAR(30) UNIQUE NOT NULL
 );
 
 CREATE TABLE role (
